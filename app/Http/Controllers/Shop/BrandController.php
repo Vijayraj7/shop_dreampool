a8abd8dec9494107d9dc95e29235ba9b276dd777
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2025-11-15 21:52:21 +0530

    init

---
+++ app/Http/Controllers/Shop/BrandController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Shop;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\BrandRequest;
+use App\Repositories\BrandRepository;
+
+class BrandController extends Controller
+{
+    /**
+     * Display a listing of the brands.
+     */
+    public function index()
+    {
+        $shop = generaleSetting('rootShop');
+
+        // Get brands
+        $brands = $shop->brands()->paginate(20);
+
+        return view('shop.brand.index', compact('brands'));
+    }
+    /**
+     * store a new brand
+     */
+    public function store(BrandRequest $request)
+    {
+        BrandRepository::storeByRequest($request);
+
+        return to_route('shop.brand.index')->withSuccess(__('Brand created successfully'));
+    }
+
+}
